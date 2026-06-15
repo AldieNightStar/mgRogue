bb194d65dde952e41d9120ad71e0677849d53844
AldieNightStar <aldie.nightstar@gmail.com>
2026-06-15 16:46:08 +0200

    Scaling

--- src/TileGraphics.cs
+++ src/TileGraphics.cs
@@ -18,14 +18,14 @@ public class TileGraphics
     public void DrawString(string text, Vector2 pos, Color? color = null)
     {
         var drawColor = color.HasValue ? color.Value : Color.White;
-        var scale = Size/_game.FontSize;
+        var scale = Size / _game.FontSize;
         _game.Batch.DrawString(
             _game.Font, text, pos * Size, drawColor, 0,
             Vector2.Zero, scale, SpriteEffects.None, 0
         );
     }
 
-    public void DrawStringMonospaced(string text, Vector2 pos, Color? color= null)
+    public void DrawStringMonospaced(string text, Vector2 pos, Color? color = null)
     {
         foreach (var (x, c) in text.Index())
         {
@@ -34,15 +34,19 @@ public class TileGraphics
         }
     }
 
-    public void DrawTile(Texture2D texture, Vector2 pos, Color? color = null, Rectangle? srcRect = null)
+    public void DrawTile(Texture2D texture, Vector2 pos, Color? color = null, Rectangle? srcRect = null, Vector2? scale = null)
     {
         var drawColor = color.HasValue ? color.Value : Color.White;
         var newPos = pos * Size;
-        var rect = new Rectangle((int)newPos.X, (int)newPos.Y, (int)Size.X, (int)Size.Y);
-        _game.Batch.Draw(
-            texture, rect, srcRect, drawColor, 0,
-            Vector2.Zero, SpriteEffects.None, 0
-        );
+        var pscale = scale.HasValue ? scale.Value : Vector2.One;
+
+        var x = (int)newPos.X;
+        var y = (int)newPos.Y;
+        var w = (int)(Size.X * pscale.X);
+        var h = (int)(Size.Y * pscale.Y);
+        var rect = new Rectangle(x, y, w, h);
+        
+        _game.Batch.Draw(texture, rect, srcRect, drawColor, 0, Vector2.Zero, SpriteEffects.None, 0);
     }
 
     public Vector2 GetScreenSizeTiles(GameWindow win)
